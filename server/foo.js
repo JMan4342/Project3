8afc4ba9de41ce260955b767fc69199e0a42bd97
St.Onge <stonge.ms@gmail.com>
2021-08-07 20:35:31 -0700

    added note to foo.js

--- server/foo.js
+++ server/foo.js
@@ -1,5 +1,10 @@
+// ??? Not sure if this code is Necessary below ???
+// ================================================
+
 const params = new URLSearchParams({
   parkCode: "arch",
 });
 
 console.log(params.toString())
+
+
